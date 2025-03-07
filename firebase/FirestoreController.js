52dba1063b3f8ea0ea2cc5e0faf16b3cc001dad1
mannisjanne <n3maja00@students.oamk.fi>
2025-03-07 12:38:52 +0200

    Added env variables

--- firebase/FirestoreController.js
+++ firebase/FirestoreController.js
@@ -1,4 +1,4 @@
-import { collection, onSnapshot, query, QuerySnaphot } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, query, QuerySnaphot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db, TODOS_REF } from "./Config";
 
@@ -6,17 +6,21 @@ export function useFireTodos(){
     const [todos, setTodos] = useState([]);
 
     useEffect(()=>{
-
         const q = query(collection(db, TODOS_REF));
 
         onSnapshot(q, querySnaphot => {
             setTodos( querySnaphot.docs.map(doc => {
                 return { id: doc.id, ...doc.data() }
             }));
-
         });
-
     }, []);
 
     return todos;
 }
+
+export function addTodo(todoText){
+
+    if(todoText.trim() !== '')
+    addDoc( collection(db, TODOS_REF), {done:false, todoText } )
+        .catch(error => console.log(error.message));
+};
